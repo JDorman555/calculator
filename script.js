4e99462934bbd75bcd0e9e803731da034c88a6ae
Joseph Dorman <JDorman555@yahoo.com>
2023-03-07 00:43:35 -0600

    Add operate function

---
+++ script.js
@@ -0,0 +1,35 @@
+function operate(num1, operator, num2) {
+  if (operator == '+') {
+    console.log(add(num1, num2));
+  }
+  else if (operator == '-') {
+    console.log(subtract(num1, num2));
+  }
+  else if (operator == '*') {
+    console.log(multiply(num1, num2));
+  }
+  else if (operator == '/') {
+    console.log(divide(num1, num2));
+  }
+  else {
+    console.log('Not a valid operator');
+  }
+}
+
+
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function subtract(num1, num2) {
+  console.log(num1 - num2);
+}
+
+function multiply(num1, num2) {
+  console.log(num1 * num2);
+}
+
+function divide(num1, num2) {
+  console.log(num1 / num2);
+}
